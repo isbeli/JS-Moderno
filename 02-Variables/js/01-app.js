ee39f14b4d710aa4974630af73e191b303a567f1
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-03-14 11:52:01 -0400

    chore: adding defining variables, with the reserved word VAR /01-app.js

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,43 @@
+//Inicializar uma variável com um valor
+var produto = "Monitor de 24 pulgadas";
+    console.log(producto);
+
+// As variáveis podem ser reatribuídas
+produto = "Monitor de 19 pulgadas";
+console.log(produto);
+
+// JavaScrip é uma linguagem de tipo diânico, nenhum tipo de dados é especificado
+produto = 20;
+console.log(producto);
+
+// Pode ser inicializado sem valor e, em seguida, atribuído
+var disponivel;
+    disponivel = true;
+    disponivel = false;
+
+//Inicializar várias variáveis
+var categoria ='Notebook',
+        marca = "Marca famosa",
+        calficação = 5;
+
+
+// As variáveis não podem ser inicializadas com números
+
+//var 99dias;
+//var 01_
+
+// As variáveis  podem ser inicializadas com letras e _ __
+var dias99;
+var _01;
+var _usuario;
+var __usuario;
+
+
+// Camel Case ex: someVar, someClass
+var nomeProduto;
+
+// Underscore ex: some_var, some__class // Esta não é comun em JavaScipt
+var nome_produto;
+
+//  PascalCase ex: ProdutoBranco, // Ele é usado apenas para definição de CLASSE
+var NomeProduto; 
