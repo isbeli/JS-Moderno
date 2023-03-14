cda56c2db8b9595f3a73c5ea3ebda66d4deef408
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-03-14 09:58:13 -0400

    chore: adding .eslint/app.js

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,3 @@
+const nome = prompt('Qual é teu nome?');
+const sobrenome = prompt('Qual e teu sobrenome?');
+document.querySelector('.contenido').innerHTML = `${nome} ${sobrenome} Esta  cursando o Curso de JS MODERNO`;
