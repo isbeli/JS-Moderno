14bddf4c341f94c7931b270bd8641314e62ab380
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-03-28 10:08:25 -0400

    chore: Adding string methods / 02-app.js

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+// Metodos para os STRING o cadeias de texto.
+
+const produto = 'Monitor 20 Pulgadas';
+
+console.log(produto);
+
+//Conhecer a cantidade de letras de o cadena de texto. 
+console.log(produto.length);
+
+// console.log(produto.indexOf('Table'));
+console.log(produto.includes('Table'));
+console.log(produto.includes('Monitor'));
+console.log(produto.includes('Pulgadas'));
