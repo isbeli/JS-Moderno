7ed03ce2454bc1658026886db6b88c6c522661de
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-03-29 18:15:25 -0400

    chore: Adding, methods to remove, whitespace at the beginning and end of a text string / 04-app.js

---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,12 @@
+const produto = '          Monitor 20 Pulgadas'
+
+console.log(produto);
+console.log(produto.length);
+
+// Elimina do inicio
+console.log(produto.trimStart() );
+console.log(produto.trimEnd() );
+
+console.log(produto.trimStart().trimEnd());
+
+console.log(produto.trim());
