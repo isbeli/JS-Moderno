6c36b39c5b9c58ef1d1ab37544bb70737c8886f8
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-03-29 19:06:21 -0400

    chore: Adding. String Methods - Repeat y Split / 06-app.js

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,20 @@
+const produto = 'Monitor 20 Pulgadas ';
+
+
+// .repetear Isso permitirá que você repita uma sequência de texto
+
+const texto = ('Em desconto'.repeat(3));
+
+console.log(texto);
+console.log(`${produto} ${texto} !!!`);
+
+// .Split, dividir um string
+const atividade = "Estou aprendendo JacaScript Moderno";
+console.log(atividade.split(" "));
+
+const hobbies = 'Ler, cantar, estudo, aprender programacao'
+console.log(hobbies.split(", "));
+
+
+const tweet = "Aprendendo JavaScript Moderno #JSModernoComIsbeli";
+console.log(tweet.split('#'));
