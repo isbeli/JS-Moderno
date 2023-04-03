bb5d9711b59e675a064d5362f58107b4c892e92a
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-04-03 10:29:56 -0400

    chore: numbers in javascript /01-app.js

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+//Creacao de nuemros
+
+const numero1= 30; //numero entero
+const numero2= "20"; //um string, tira as aspas e vai ter um numero.
+const numero3= 20.20; //numero float
+const numero4= .102320; // 
+const numero5= -20; // numero negativo
+
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
